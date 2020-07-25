e5018bd02458d0c426219eaa3963931a6895f287
Jules <julia@jonckheere.nl>
2020-07-25 10:55:19 -0800

    3d gedeelte is af, alleen nog shader, particles en 2d gedeelte

---
+++ BPW2Game/Assets/Scripts/CutsceneEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneEnter : MonoBehaviour
+{
+	public Fade FadeScript; 
+	public GameObject thePlayer;
+	public GameObject cutsceneCam;
+
+	void OnTriggerEnter(Collider other)
+	{
+		cutsceneCam.SetActive(true);
+		thePlayer.SetActive(false);
+		FadeScript.StartCoroutine(FadeScript.StartFade());
+	}
+}
---
+++ BPW2Game/Assets/Scripts/Fade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    private Image image;
+
+        [SerializeField] private float delay, time;
+
+        public LeanTweenType EaseType;
+
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+            Color color = image.color;
+            image.color = new Color(color.r, color.g, color.b, 0);
+        }
+
+        public IEnumerator StartFade()
+        {
+            LeanTween.alpha(image.rectTransform, 1, time).setEase(EaseType).setDelay(delay).setIgnoreTimeScale(true);
+            
+           yield return new WaitForSeconds(time + delay);
+            SceneManager.LoadSceneAsync(1);
+        }
+}
